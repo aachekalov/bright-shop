786df61f5ce5f2aef4c099aa8bbd078cb7cfb573
Alexey Chekalov <a.chekalov@prosigma.ru>
2018-08-17 11:15:54 +0400

    setup

--- config/web.php
+++ config/web.php
@@ -5,6 +5,9 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+	'language' => 'ru',
+	'name' => 'Учет в магазине',
+	'homeUrl' => '/product/index',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'aliases' => [
@@ -43,14 +46,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
